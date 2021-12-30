99b04bbe54e1b4f58a641c8cee3a84960ea8b476
zataara <bryantmac@gmail.com>
2021-12-30 08:58:34 -0800

    added ensureAdmin middleware for private routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,26 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/** Middleware to use when they be logged in as an admin user.
+ *
+ *  If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if(!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch(err) {
+    return next(err);
+  }
+}
+
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin
 };
